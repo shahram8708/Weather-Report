4c6285e908bc23bd14e537fe2d67981f0b80fbab
Shah Ram <152763547+shahram8708@users.noreply.github.com>
2024-11-18 10:30:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.post('/getWeather', async (req, res) => {
     const city = req.body.city;
 
-    const apiKey = 'b8993812d0f0fea1b9dabe09ca873a6f';
+    const apiKey = 'API_Key';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     try {
@@ -27,7 +27,7 @@ app.post('/getWeather', async (req, res) => {
 app.get('/getWeather', async (req, res) => {
     const { lat, lon } = req.query;
 
-    const apiKey = 'b8993812d0f0fea1b9dabe09ca873a6f';
+    const apiKey = 'API_Key';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     try {
